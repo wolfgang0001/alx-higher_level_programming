ab3d6ae9ac7824b1f47e28a29e1b275f88ac91ea
wolfgang0001 <chukwunonso53@gmail.com>
2024-02-14 11:55:00 -0800

    follow

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- creates the table id_not_null
+CREATE TABLE `id_not_null` (`id` int DEFAULT 1, `name` VARCHAR(256))
---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- creates the table unique_id
+CREATE TABLE `unique_id` (`id` int DEFAULT 1 UNIQUE, `name` VARCHAR(256))
